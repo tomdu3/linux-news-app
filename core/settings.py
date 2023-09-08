5eff4c0d596d03568f8343d172442fe2e26193a3
Tomislav Dukez <tomdu3@ymail.com>
2023-09-08 21:48:43 +0000

    Fix load environment variables

--- core/settings.py
+++ core/settings.py
@@ -15,16 +15,17 @@ import os
 
 
 # load environment variables file
-
 import dj_database_url
 from dotenv import load_dotenv
+
+if os.path.isfile('.env'):
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
-load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
