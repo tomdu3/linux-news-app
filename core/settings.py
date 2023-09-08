53253427bf5f6cf04043c1ca50bddc713ea7c8c2
Tomislav Dukez <tomdu3@ymail.com>
2023-09-08 21:59:04 +0000

    fix allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['https://books-4-life-2d26bdf04dec.herokuapp.com/','8000-tomdu3-booksforlife-faydvwtuct5.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = [
+    'books-4-life-2d26bdf04dec.herokuapp.com/',
+    '8000-tomdu3-booksforlife-faydvwtuct5.ws-eu104.gitpod.io'
+    ]
 
 
 # Application definition
@@ -86,6 +89,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     # 'default': {
