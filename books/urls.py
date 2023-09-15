b2071bb1107a1a0b1548a7e2b2763b927cea8e4d
Tomislav Dukez <tomdu3@ymail.com>
2023-09-15 11:56:27 +0100

    add user_page template and view

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('books/user_page', views.user_page, name='user_page'),
     ]
--- books/views.py
+++ books/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'books/home.html')
+
+def user_page(request):
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'books/user_page.html', context=context)
