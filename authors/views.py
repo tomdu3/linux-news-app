8d34e95c14424559f80fabff525b5671411f3d14
Tomislav Dukez <tomdu3@ymail.com>
2023-10-01 21:39:07 +0100

    Fix double code for update profile view and better validation

--- authors/views.py
+++ authors/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth import (
+    authenticate, login, logout, update_session_auth_hash)
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
 from django.contrib import messages
 from .forms import SignUpForm, UpdateProfileForm, ContactForm
 from django.contrib.auth.models import User
 from .models import UserProfile
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import json
 
 
-# Create your views here.
-
-
+# User login authentication view
 def login_user(request):
     if request.method == 'POST':
         username_or_email = request.POST["username_or_email"]
@@ -33,7 +30,7 @@ def login_user(request):
             
         if user is not None:
             login(request, user)
-            # Redirect to a success page.
+            # Redirect to a home page.
             messages.success(request, ('You are logged in. Congrats!'), extra_tags='success')
             return redirect('/')
         else:
@@ -43,11 +40,14 @@ def login_user(request):
     else:
         return render(request, 'authentication/login.html', {})
 
+# User logout view
 def logout_user(request):
     logout(request)
     messages.success(request, ('You have been logged out!', ), extra_tags='info')
     return redirect('/')
 
+
+# User registration view
 def register_user(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -66,6 +66,7 @@ def register_user(request):
     return render(request, 'authentication/signup.html', context)
 
 
+# User profile view
 @login_required
 def user_profile(request):
     user = request.user
@@ -74,58 +75,7 @@ def user_profile(request):
         }
     return render(request, 'user_profile.html', context)
 
-@login_required
-def update_profile(request):
-
-    # Get the user's profile instance
-    try:
-        user_profile = UserProfile.objects.get(user=request.user)
-    except UserProfile.DoesNotExist:
-        # If the user's profile doesn't exist, create a new one
-        user_profile = UserProfile(user=request.user)
-        user_profile.save()
-
-    if request.method == 'POST':
-        form = UpdateProfileForm(request.POST, instance=request.user)
-
-        if form.is_valid():
-            user = form.save()
-
-            # Check if the user entered a new password
-            new_password = form.cleaned_data.get('new_password1')
-            new_password_confirm = form.cleaned_data.get('new_password2')
-
-            if new_password != new_password_confirm:
-                messages.error(request, ('The two password fields didn\'t match.'), extra_tags='danger')
-                return redirect('update_profile')
-
-            if new_password:
-                # Validate the new password
-                try:
-                    validate_password(new_password)
-                except ValidationError as errors:
-                    for error in errors.error_list:
-                        messages.error(request, error, extra_tags='danger')
-                    return redirect('update_profile')
-
-                # Update the user's password and keep them logged in
-                user.set_password(new_password)
-                user.save()
-                update_session_auth_hash(request, user)
-
-            messages.success(request, 'Your profile was successfully updated!', extra_tags='success')
-            return redirect('user_profile')
-        else:
-            # Add error messages for form validation errors
-            for field, errors in form.errors.items():
-                for error in errors:
-                    messages.error(request, (f'Error in {field}: {error}'), extra_tags='danger')
-
-    else:
-        form = UpdateProfileForm(instance=request.user)
-
-    return render(request, 'user_profile_update.html', {'form': form})
-
+# Update Profile View
 @login_required
 def update_profile(request):
     # Get the user's profile instance
@@ -167,8 +117,15 @@ def update_profile(request):
             # Check if the user is changing the email address
             new_email = form.cleaned_data.get('email')
             if new_email and new_email != user.email:
-                # Perform email validation or checks here if needed
-                # Example: Check if the new email address is unique
+                email_validator = EmailValidator(message='Enter a valid email address.')
+
+                try:
+                    email_validator(new_email)
+                except ValidationError:
+                    form.add_error('email', 'Invalid email format.')
+                    return render(request, 'user_profile_update.html', {'form': form})
+
+                # Check if the new email address is unique
                 if User.objects.filter(email=new_email).exclude(username=user.username).exists():
                     form.add_error('email', 'This email address is already in use.')
                     return render(request, 'user_profile_update.html', {'form': form})
@@ -184,40 +141,13 @@ def update_profile(request):
             user.save()  # Save the updated User model
             user_profile.save()  # Save the updated UserProfile model
 
-            messages.success(request, 'Your profile was successfully updated!')
+            messages.success(request, ('Your profile was successfully updated!'), extra_tags='success')
             return redirect('user_profile')
-        else:
-            # Add error messages for form validation errors
-            for field, errors in form.errors.items():
-                for error in errors:
-                    messages.error(request, f"Error in {field}: {error}")
-
-            messages.error(request, 'There was an error in the form. Please correct the errors.')
 
     else:
         form = UpdateProfileForm(instance=request.user)
 
     return render(request, 'user_profile_update.html', {'form': form})
-
-
-
-def validate_password(password):
-    errors = []
-
-    if len(password) < 8:
-        errors.append(ValidationError("Password must be at least 8 characters long."))
-
-    if not any(c.isupper() for c in password):
-        errors.append(ValidationError("Password must contain at least one uppercase letter."))
-
-    if not any(c.islower() for c in password):
-        errors.append(ValidationError("Password must contain at least one lowercase letter."))
-
-    if not any(c.isdigit() for c in password):
-        errors.append(ValidationError("Password must contain at least one digit."))
-
-    if errors:
-        raise ValidationError(errors)
 
 
 @login_required
